d881e3943691c68df80d135b72994c3f39a0002d
your-github-usernamegit <kavya.singh@shunyavkash.com>
2025-06-23 18:36:36 +0530

    all completed ui strucher with res

---
+++ assests/js/main.js
@@ -0,0 +1,7 @@
+<script>
+  AOS.init({
+    duration: 1000, // animation duration in ms
+    once: true,     // animate only once
+    offset: 100     // how far from top before triggering
+  });
+</script>
